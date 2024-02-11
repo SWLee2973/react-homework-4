a4b8236decbb48e49801759ca1eb69b2d2daef51
SWLee2973 <kek2973@naver.com>
2024-02-11 18:05:58 +0900

    [modify] 버튼 기본 스타일 수정

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
 import { memo } from 'react';
 
 const buttonTheme = {
-  zinc: 'hover:bg-zinc-900  bg-zinc-700 text-white',
-  white: 'hover:bg-slate-300  bg-white text-zinc-700',
+  zinc: 'w-72 h-10 hover:bg-zinc-900  bg-zinc-700 text-white',
+  white: 'w-72 h-10 hover:bg-slate-300  bg-white text-zinc-700',
 };
 
 function Button({
@@ -15,7 +15,7 @@ function Button({
   return (
     <button
       type={type}
-      className={`w-72 h-10 rounded-sm text-md ${styleClass} ${buttonTheme[theme]}`}
+      className={`rounded-sm text-md ${styleClass} ${buttonTheme[theme]}`}
       {...restProps}
     >
       {children}
